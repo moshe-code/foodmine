b8778d39c1b8ac7c9cf3f2870ce9924fbcc391af
Moshe <moshesweiner@.com>
2022-08-16 20:15:51 +0300

    Search Comeplete

---
+++ frontend/src/app/shared/models/Food.ts
@@ -0,0 +1,11 @@
+export class Food {
+  id!: String;
+  name!: String;
+  price!:number;
+  tags?:string[];
+  favorite!:boolean;
+  stars!:number;
+  imageUrl!:string;
+  origins!:string[];
+  cookTime!:string
+}
